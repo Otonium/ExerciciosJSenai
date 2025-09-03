0cc80a067c41f19792cc90f36e9ce8d958e36c4f
ANTONIO CARLOS VIEIRA SANTOS <ola.antoniocaros@gmail.com>
2025-09-03 10:49:54 -0300

    commitando

---
+++ script.js
@@ -0,0 +1,109 @@
+//! Exercicio 1
+//*  Verificaar maioridade
+//* Usuario insere valor
+//* logica com if else
+//* 
+//*Usuario digita idade (input, numeor int)
+//* O que é variavel aqui? idade
+//*Logica idade: Idade >= 18?
+//*Se idade >= 18 faça
+//*  Escreve a
+//*senao faça
+//*  Escreve b
+
+/*let idade = prompt("Digite a idade: ")
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+    alert("Maior de idade")
+} else if (idade < 18) {
+    console.log("Menor de idade")
+    alert("Menor de idade")
+} else {
+    console.log("Invalido")
+    alert("Invalido")
+}*/
+
+//! Exercicio 2
+//* Verificar se é positivo ou negativo
+//* Se é maior 0 é positivo, se menor ou igual, não é
+//* Usuario insere valor, logica com if else
+//* 
+//*Usuario digita numero
+//*O que é variavel aqui? Numero digitado
+//*Logica: Numero é <= ou > que zero?
+//*Se num > 0 faça
+//* Escreva positivo
+//*Se <= 0 faça
+//* Escreva negativo
+//?? Numero isolado
+/*let num = prompt("Digite um numero interiro (Z)")
+let sinal = "-"
+
+if (sinal) {
+    console.log("Negativo")
+    alert("Negativo")
+} else {
+    console.log("Positivo")
+    alert("Positivo")
+}*/
+
+//?? Opercações
+/*if (num > 0) {
+    console.log("Positivo")
+    alert("Positivo")
+} else if (num <= 0) {
+    console.log("Positivo")
+    alert("Positivo")
+}*/
+
+//! Exercicio 3
+//* Aprovação de aluno
+//* Nota vai de 0 até 100 (for ou condição?)
+//* Nota >= 60, aprovado, senao reprovado
+
+/*let nota = prompt("Digite nota do aluno: ")
+
+if (nota > 100) {
+    console.log("Invalido")
+    alert("Invalido")
+} else if (nota >= 60) {
+    console.log("Aprovado")
+    alert("Aprovado")
+} else {
+    console.log("Reprovado")
+    alert("Reprovado")
+}*/
+
+//! Exercicio 4
+//* Verificar se é positivo ou negativo
+//* Se é maior 0 é positivo, se menor, não é
+//* Usuario insere valor, logica com if else
+//* 
+//*Usuario digita numero
+//*O que é variavel aqui? -> Numero digitado
+//*Logica: Numero é <, >, ou igual a zero?
+//*Se num > 0 faça
+//* Escreva positivo
+//*Se <= 0 faça
+//* Escreva negativo
+//?? Numero isolado
+/*let num = prompt("Digite um numero interiro (Z)")
+let sinal = "-"
+
+if (sinal) {
+    console.log("Negativo")
+    alert("Negativo")
+} else {
+    console.log("Positivo")
+    alert("Positivo")
+}
+
+//?? Opercações
+if (num > 0) {
+    console.log("Positivo")
+    alert("Positivo")
+} else if (num <= 0) {
+    console.log("Positivo")
+    alert("Positivo")
+}*/
